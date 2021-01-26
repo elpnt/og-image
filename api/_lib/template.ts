f86b3860e41b814b45ff95d1d55d96c1f5f4801d
Matsuoka <quantumular@gmail.com>
2021-01-27 01:25:59 +0900

    bug fix

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -6,12 +6,6 @@ const twemoji = require('twemoji')
 const twOptions = { folder: 'svg', ext: '.svg' }
 const emojify = (text: string) => twemoji.parse(text, twOptions)
 
-const rglr = readFileSync(
-  `${__dirname}/../_fonts/NotoSans-Regular.woff2`
-).toString('base64')
-const bold = readFileSync(
-  `${__dirname}/../_fonts/NotoSans-Bold.woff2`
-).toString('base64')
 const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString(
   'base64'
 )
